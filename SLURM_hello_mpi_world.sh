a09bb6c70ed3a37e4bb44fc402dc030582eeff91
Konrad Simon <konrad.simon@uni-hamburg.de>
2020-11-24 13:09:36 +0000

    changed submission script

--- SLURM_hello_mpi_world.sh
+++ SLURM_hello_mpi_world.sh
@@ -1,28 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=job_hello-mpi-world_myMPI
-#SBATCH --output=job_hello-mpi-world_myMPI.out
-#SBATCH --error=job_hello-mpi-world_myMPI.err
+#SBATCH --job-name=job_hello-mpi-world
+#SBATCH --output=job-hello_mpi_world-%j.out
+#SBATCH --error=job-hello_mpi_world-%j.err
 
-#SBATCH --partition=uni-all
-#SBATCH --nodes=10
-#SBATCH --ntasks-per-node=12
+#SBATCH --partition=pi4-cluster-all
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=4
 
-#SBATCH --mail-user=konrad.simon@uni-hamburg.de
-#SBATCH --mail-type=ALL
+# Enter submission  directory
+cd $SLURM_SUBMIT_DIR
 
-# init dealii
-source /scratch/cen/numgeo/init_spack.sh
-spack load openmpi@3.1.6%gcc@8.1.0 arch=linux-debian8-x86_64
+# Print the node that starts the process
+echo "Master node: $(hostname)"
 
-# below your job commands :
-#cd /scratch/cen/numgeo/Code_Konrad/cluster_test/
-#FILE=/scratch/cen/numgeo/Code_Konrad/cluster_test/hello-mpi-world
-#if test -f "$FILE"; then
-#    echo "$FILE exists. Removing and recompiling..."
-#    rm hello-mpi-world
-#fi
-#which mpicc
-#mpicc -v hello-mpi-world.c -o hello-mpi-world
-#echo "Testprogram recompiled."
-
-mpirun  /scratch/cen/numgeo/Code_Konrad/cluster_test/hello-mpi-world
+mpirun  hello_mpi_world
